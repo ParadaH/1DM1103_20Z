f5dfe97f22900f7dc83d3dcc0a82cdf1cd504ac9
ParadaH <hubert@DESKTOP-TFB4PET.localdomain>
2020-10-22 16:29:35 +0200

    Zadanie domowe 22.10

--- sem3/p1.c
+++ sem3/p1.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int main(int argc, char *argv[])
     {
         float v;
         float a1;
         float a2;
-        a1 = atof("4");
-        a2 = atof("6");
+        a1 = atof(argv[1]);
+        a2 = atof(argv[2]);
         v = a1 + a2;
-        printf("[%s] Suma: %f\n", argv[0], v);
+        printf("[%s] Suma: %f\n", argv[0], atof(argv[1]) + atof(argv[2]));
     }
